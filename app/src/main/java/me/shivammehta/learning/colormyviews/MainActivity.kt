51c6904ba4fd651f4b3490992bfa67a982867592
Shivam Mehta <shivam.mehta007@gmail.com>
2019-09-29 14:36:22 +0300

    Adding buttons with color changes

--- app/src/main/java/me/shivammehta/learning/colormyviews/MainActivity.kt
+++ app/src/main/java/me/shivammehta/learning/colormyviews/MainActivity.kt
@@ -17,7 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableView : List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text,
-                                                box_five_text, constraint_layout)
+                                                box_five_text, constraint_layout, red_button, yellow_button,
+                                                green_button)
 
         for(view in clickableView) {
             view.setOnClickListener { makeColored(it) }
@@ -32,6 +33,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(getColor(android.R.color.holo_green_light))
             R.id.box_four_text -> view.setBackgroundColor(getColor(android.R.color.holo_blue_bright))
             R.id.box_five_text -> view.setBackgroundColor(getColor(android.R.color.holo_green_dark))
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
 
